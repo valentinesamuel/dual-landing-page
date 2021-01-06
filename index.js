33f339169c8c226e4c8f2b587575be4535a98c13
Valentine <valentinesamuel2580@gmail.com>
2021-01-06 13:44:00 +0100

    (FEAT:I am so happy that i finshed this project

---
+++ index.js
@@ -0,0 +1,17 @@
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const wrapper = document.querySelector("#wrapper");
+
+// left.addEventListener("mouseenter", () => left.classList.add("hover-left"));
+
+left.addEventListener("mouseenter", () => wrapper.classList.add("hover-left"));
+left.addEventListener("mouseleave", () =>
+  wrapper.classList.remove("hover-left")
+);
+
+right.addEventListener("mouseenter", () =>
+  wrapper.classList.add("hover-right")
+);
+right.addEventListener("mouseleave", () =>
+  wrapper.classList.remove("hover-right")
+);
